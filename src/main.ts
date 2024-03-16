a1bcdac040fba4f7d1c01e7dcf896889ec699f65
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2024-03-16 10:56:26 -0600

    added new update prompt

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,42 @@
 import { getVersion } from "@tauri-apps/api/app";
 import { appWindow } from "@tauri-apps/api/window";
 
-window.addEventListener("DOMContentLoaded", () => {
+import { checkUpdate, installUpdate, onUpdaterEvent } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+
+window.addEventListener("DOMContentLoaded", async () => {
     const version_span = document.getElementById("version-span");
     setTimeout(async () => {
         version_span!.innerHTML = await getVersion();
     }, 150);
 
     document.getElementById("kill-app-button")!.addEventListener("click", () => appWindow.close());
+
+    //--
+
+    await onUpdaterEvent(({ error, status }) => {
+        console.log("Updater event", error, status);
+    });
+
+    try {
+        const { shouldUpdate, manifest } = await checkUpdate();
+
+        if (shouldUpdate) {
+            console.log(`Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`);
+            showAlert(async () => {
+                await installUpdate();
+                await relaunch();
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
 });
+
+function showAlert(callback: ((this: GlobalEventHandlers, ev: MouseEvent) => any) | null) {
+    var modal = document.getElementById("updateAlert");
+    modal!.style.display = "block";
+
+    var updateBtn = document.getElementById("updateBtn");
+    updateBtn!.onclick = callback;
+}
